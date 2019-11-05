f6fb8cc4c97f53841614ca0f1f4e16477006f532
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-05 20:04:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
+require 'pry'
 #write your code here
 
-def countdown
+def countdown(seconds)
+  count = seconds
+  its_time = 0
+  while count > its_time do
+    p "#{count} SECONDS(S)!"
+    count -= 1
+  end
+
+"HAPPY NEW YEAR!" if count == its_time
+end
+
+def countdown_with_sleep(seconds)
+  count = seconds
+  its_time = 0
+  while count > its_time do
+    p "#{count} SECONDS(S)!"
+    count -= 1
+    sleep(1)
+  end
+
+"HAPPY NEW YEAR!" if count == its_time
+
 end
