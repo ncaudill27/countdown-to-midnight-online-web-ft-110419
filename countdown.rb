28cc83a51c07b08a89dfea47f0bca88bf057da8e
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-05 20:52:16 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
+def countdown(seconds)
+  count = seconds
+  
+  until count == 0 do
+    puts("#{count} SECOND(S)!")
+    count -= 1
+  end
 
-def countdown
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  count = seconds
+  
+  while count > 0 do
+    p "#{count} SECOND(S)!"
+    count -= 1
+    sleep(1)
+  end
+
+"HAPPY NEW YEAR!"
 end
